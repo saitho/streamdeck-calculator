40bad331c93676711e6fa1b4a2600d93a49c7855
Mario Lubenka <mario.lubenka@googlemail.com>
2021-12-24 17:07:29 +0100

    feat: number button

---
+++ streamdeck-calculator/Actions/NumberAction.cs
@@ -0,0 +1,101 @@
+﻿using BarRaider.SdTools;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+/**
+ * Number Action
+ * 
+ * This action can be set multiple times by the user, and they can configure a number.
+ * If the button is pressed, the number will be as new digit to the current number in
+ * "currentNumber" memory storage.
+ **/
+namespace saitho.Calculator.Actions
+{
+    [PluginActionId("com.saitho.calculator.number")]
+    public class NumberAction : PluginBase
+    {
+        private class PluginSettings
+        {
+            public static PluginSettings CreateDefaultSettings()
+            {
+                PluginSettings instance = new PluginSettings
+                {
+                    Number = 0
+                };
+                return instance;
+            }
+
+            [JsonProperty(PropertyName = "number")]
+            public int Number { get; set; }
+        }
+
+        #region Private Members
+
+        private readonly PluginSettings settings;
+
+        #endregion
+        public NumberAction(SDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+            if (payload.Settings == null || payload.Settings.Count == 0)
+            {
+                this.settings = PluginSettings.CreateDefaultSettings();
+            }
+            else
+            {
+                this.settings = payload.Settings.ToObject<PluginSettings>();
+            }
+        }
+
+        public override void Dispose()
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, $"Destructor called");
+        }
+
+        public override void KeyPressed(KeyPayload payload)
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, $"Key Pressed - Number: {this.settings.Number}");
+            //string operation = MemoryStore.getInstance().get("operation");
+            //if (operation == null)
+            //{
+            //    Connection.ShowAlert();
+            //    return;
+            //}
+
+            // add to number in memory
+            int currentNumber = 0;
+            try
+            {
+                currentNumber = int.Parse(MemoryStore.getInstance().get("currentNumber"));
+            } catch { }
+
+            int newNumber = currentNumber * 10 + this.settings.Number;
+            Logger.Instance.LogMessage(TracingLevel.INFO, $"Key Pressed - Old number: {currentNumber}, new number: {newNumber}");
+            MemoryStore.getInstance().set("currentNumber", newNumber.ToString());
+            Connection.ShowOk();
+        }
+
+        public override void KeyReleased(KeyPayload payload) { }
+
+        public override void OnTick() { }
+
+        public override void ReceivedSettings(ReceivedSettingsPayload payload)
+        {
+            Logger.Instance.LogMessage(TracingLevel.INFO, "ReceivedSettings");
+            Tools.AutoPopulateSettings(settings, payload.Settings);
+            SaveSettings();
+        }
+
+        public override void ReceivedGlobalSettings(ReceivedGlobalSettingsPayload payload) { }
+
+        #region Private Methods
+
+        private Task SaveSettings()
+        {
+            return Connection.SetSettingsAsync(JObject.FromObject(settings));
+        }
+
+
+        #endregion
+    }
+}
